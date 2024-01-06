9733a9b4e8ed7cf7e9ace1dad6001deb692f19d9
collins-self <collinsnater03@gmail.com>
2024-01-06 12:09:54 -0500

    init

---
+++ src/main/java/com/collinsself/servlets/ExecuteServlets.java
@@ -0,0 +1,11 @@
+package main.java.com.collinsself.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/execute")
+public class ExecuteServlets extends HttpServlet {
+}
